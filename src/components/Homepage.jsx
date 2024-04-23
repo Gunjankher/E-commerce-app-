c4f54b4b61c4ebe5ee0392f31ea2fd043f6dcbe0
Gunjankher <gkhercode@gmail.com>
2024-04-23 14:27:50 +0530

    ..............

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Carousel from './Carousel'
+import Carousel2 from './Carousel2'
 
 function Homepage() {
   return (
@@ -7,9 +8,12 @@ function Homepage() {
 
     <div className='min-w-[1000px] max-w-[1600px]  bg-blue-500'> navbar</div>
     <div className='min-w-[1000px] max-w-[1600px] bg-gray-500'> categories</div>
-
     <Carousel />
-    <img  className = "w-[100px]"src="../images/Carousel/banner.png" alt="" />
+    <div className='bg-slate-600'>hi</div>
+    <div>
+<Carousel2 />
+
+    </div>
     </div>
   )
 }
