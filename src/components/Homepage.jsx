e32bc42ec10239f34b0816e33de1dde7ba516ab6
Gunjankher <gkhercode@gmail.com>
2024-04-24 13:57:13 +0530

    ...........

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Carousel from './Carousel'
 import CarouselProduct from './CarouselProduct'
+import Navbar from './Navbar'
 
 function Homepage() {
   return (
-    <div className='bg-amazonclone-background'>Homepage 
+    <div className='bg-amazonclone-background'>
+      <Navbar />
     <Carousel />
     <CarouselProduct />
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import {ShoppingCartIcon} from '@heroicons/react/24/outline'
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+ <header className='min-w-[1000px]'>
+<div className='bg-amazonclone h-[60px]'>
+
+<div className='flex'>
+    {/* left */}
+<div className='flex items-center mt-2 pl-5'>
+    <img className='w-[100px]' src="../images/amazon.png" alt="" />
+
+<div className='text-white pl-4'>
+    <div className='text-xs xl:text-sm"'>Deliver to</div>
+    <div className='text-sm xl:text-base font-bold'>Ahemdabad</div>
+</div>
+
+</div>
+
+
+    {/* middle */}
+<div className='text-white grow'>Search</div>
+{/* right */}
+<div className='flex items-center'>
+
+<div className='text-white pl-4 pr-4'>
+<div>Hello</div>
+<div>Sign in </div>
+</div>
+
+<div className='text-white pl-4 pr-4 '>
+    <div>Return</div>
+    <div>&Orders</div>
+</div>
+
+<div>
+
+</div>
+<div className="flex pr-3 pl-3 text-white">
+              <ShoppingCartIcon className="h-[48px]" />
+              <div className="relative">
+                <div className="absolute right-[9px] font-bold m-2 text-orange-400">0
+                </div>
+              </div>
+              <div className="mt-7 text-xs xl:text-sm font-bold">Cart</div>
+            </div>
+</div>
+
+
+
+</div>
+
+</div>
+
+ </header>
   )
 }
 
