7beabdb8c1e6f120701e077c3df15ed03921a0b4
Gunjankher <gkhercode@gmail.com>
2024-04-25 14:38:39 +0530

    .......................

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -23,11 +23,27 @@ if(!product?.title)return <h1>loading............</h1>
 
   return (product && 
     (
+<div className='h-screen bg-amazonclone-background'>
+  <div className='max-w-[1600px] min-w-[1000px]'>
+    <div className='grid grid-cols-10'>
+{/* left     */}
+<div className='col-span-3 bg-white m-auto p-8'>
+  <img src={`${product.image}`}  />
+</div>
+{/* middle */}
+<div className='col-span-5 bg-green-400'>
+g
+</div>
+{/* right */}
+<div className='col-span-2 bg-blue-400'>
+b
+</div>
 
-      <div>{product.title}
 
-        <img src={`${product.image}`} alt="" />
-      </div>
+    </div>
+  </div>
+
+</div>
     )
   )
 }
