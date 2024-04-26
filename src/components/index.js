4c8c65a0a5b8e37f30d1fcb1f74bd78ccf2d4d8d
Gunjankher <gkhercode@gmail.com>
2024-04-26 11:16:17 +0530

    .............

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Search from "./Search";
 import ProductPage from "./ProductPage";
 import Checkout from "./Checkout";
 import SearchResults from "./SearchResults";
-import ProductDetails from "./ProductDetails";
+import ProductDetails from "../components/";
 
 export {
     Homepage,
