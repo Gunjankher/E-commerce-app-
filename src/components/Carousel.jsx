628cd7619c7eb1cdfd2efca8eefc6ededbfd9450
Gunjankher <gkhercode@gmail.com>
2024-04-29 10:51:32 +0530

    Formatting

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,41 +1,41 @@
 import React from 'react'
-import { Swiper,SwiperSlide } from 'swiper/react'
-import { Navigation,Pagination,Autoplay } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
 import "swiper/css"
 import "swiper/css/navigation"
 
 function Carousel() {
   return (
     <div className='h-[700px]'>
-<Swiper
-className='h-[50%]'
-navigation = {true}
-spaceBetween={0}
-pagination = {{
-    clickable : true
-}}
-autoplay = {{
-    delay :3000
-}}
-modules={[Navigation,Autoplay,Pagination]}
->
+      <Swiper
+        className='h-[50%]'
+        navigation={true}
+        spaceBetween={0}
+        pagination={{
+          clickable: true
+        }}
+        autoplay={{
+          delay: 3000
+        }}
+        modules={[Navigation, Autoplay, Pagination]}
+      >
 
 
-<SwiperSlide><img src="../images/carousel_1.jpg" alt="" /></SwiperSlide>
-<SwiperSlide className="bg-black">
+        <SwiperSlide><img src="../images/carousel_1.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide className="bg-black">
           <video controls>
             <source src={"../images/carousel_vid.mp4"} type="video/mp4" />
           </video>
         </SwiperSlide>
-<SwiperSlide><img src="../images/carousel_2.jpg" alt="" /></SwiperSlide>
-<SwiperSlide><img src="../images/carousel_3.jpg" alt="" /></SwiperSlide>
-<SwiperSlide><img src="../images/carousel_4.jpg" alt="" /></SwiperSlide>
-<SwiperSlide><img src="../images/carousel_5.jpg" alt="" /></SwiperSlide>
-</Swiper>
+        <SwiperSlide><img src="../images/carousel_2.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="../images/carousel_3.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="../images/carousel_4.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="../images/carousel_5.jpg" alt="" /></SwiperSlide>
+      </Swiper>
 
-<div className='bg-stone-300 h-[300px] w-full'></div>
+      <div className='bg-stone-300 h-[300px] w-full'></div>
     </div>
-    
+
   )
 }
 
